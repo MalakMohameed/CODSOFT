6fa0ab5717293d86d63ee88ec0c0c77ce0cd8e3a
PatrickBatmaan <malakmohamed_2004@hotmail.com>
2024-02-29 20:14:14 +0200

    Calculator Web App
    fully functional web calculator

---
+++ Task03/Function.js
@@ -0,0 +1,22 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    var inputValue = document.getElementById('display').value.trim();
+
+    if(inputValue == ""){
+        alert("Please enter your values.");
+        return;
+    }
+    try {
+        var result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
